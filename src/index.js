23d887a0cf5fb7d3cbac1a34df81c5a1a7d42bf4
Sheshan Tharindu Herath <136969057+sheshan200@users.noreply.github.com>
2023-12-13 08:27:46 +0530

    finish

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+import { FavoritesContextProvider } from './store/favorites-context';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <FavoritesContextProvider >
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  </FavoritesContextProvider >
+ 
+);
